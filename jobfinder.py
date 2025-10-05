54eeb74ecdb9bbb877a4b4e80a25277cf52c9218
Serin Kalpeshkumar Patel <40085170+serinpatel@users.noreply.github.com>
2025-10-05 14:48:36 -0400

    Update jobfinder.py

--- jobfinder.py
+++ jobfinder.py
@@ -2,7 +2,7 @@ import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from serpapi.google_search_results import GoogleSearch
+from serpapi.google_search import GoogleSearch
 import requests
 import yaml
 from datetime import datetime
